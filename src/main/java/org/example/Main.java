4d7e46df25576ecd90b490e345a3047c87b65bc9
Fawato <109968429+Fawato@users.noreply.github.com>
2022-08-05 20:49:54 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,18 @@ public class Main {
     public static void main(String[] args) {
 
         String password = "new";
-        System.out.println("Hello world!");
+        
+        
+        
+        
+        String one = "a";
+        
+        String two = "b";
+        
+        
+        String passWord = "no";
+        System.out.println("no");
+
         System.out.println(password);
     }
 }
